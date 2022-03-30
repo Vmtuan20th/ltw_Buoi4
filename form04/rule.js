ee3c1338b917a69c4123f8be78285b2f20bede5b
Vmtuan20th <99734813+Vmtuan20th@users.noreply.github.com>
2022-03-30 21:43:35 +0700

    Add files via upload

---
+++ form04/rule.js
@@ -0,0 +1,20 @@
+function isTextNull(){
+
+    var name = f.txtFullname.value;
+    var day = f.cbbDay.value;
+    var month = f.cbbMonth.value;
+    var year = f.cbbYear.value;
+
+    var carrer = f.cbbCarrer.value;
+
+
+
+
+    if(name == '' || day == '' || month == '' || year == '' || carrer == '')
+    {
+        alert('Please fill in all the information');
+        return false;
+    }
+
+    return true;
+}
